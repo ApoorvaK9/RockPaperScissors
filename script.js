cc9f54e763dd57974693b3774335451c5a7519ce
Apoorva <apoorvakarekar@gmail.com>
2024-07-02 13:35:33 +0200

    Get computer choice between Rock,Paper,Scissors

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-// function creates a randeom number between 1,2,3
-
+// function creates a random number between 1,2,3
 function randomNumber() {
     return (Math.floor( 1 + ( Math.random() * 3 )));
 }
 
+// function sets computer choice between rock, paper and scissors
+function getComputerChoice() {
+    let randomNum = randomNumber();
+    if (randomNum === 1)
+        return "Rock";
+    else if (randomNum === 2)
+        return "Paper";
+    else if (randomNum === 3)
+        return "Scissors";
+}
+
+console.log(getComputerChoice());
