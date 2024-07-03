0a3b8af5defcd2bf131ad3fe73f10efeadd8fff6
Apoorva <apoorvakarekar@gmail.com>
2024-07-03 18:58:52 +0200

    Make the prompt given to user more specific

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function getComputerChoice() {
 
 // function to get user input
 function getUserChoice(){
-    let userInput = prompt("Please choose one of Rock,Paper or Scissors");
+    let userInput = prompt("Please write your choice below (Rock,Paper or Scissors)");
     let userInputLowerCase = userInput.toLowerCase();
     if (userInputLowerCase === "rock" )
         return "Rock";
