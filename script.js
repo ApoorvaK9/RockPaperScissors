8286332bf31f11a13ad5e87896baa6ede9bff038
Apoorva <apoorvakarekar@gmail.com>
2024-08-09 16:37:34 +0200

    Display score & winner when a player reaches 5 points

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 let userScore = 0;
 let computerScore = 0;
-let winner = (userPoints,computerPoints) => {
-    (userPoints > computerPoints) ? displayResult(`Congrats! You win! Celebrate!`)
-    :(userPoints < computerPoints) ? displayResult(`You lose!\nBetter luck next time`)
-    :displayResult("Its a tie!")};
-    
 
 // function creates a random number between 1,2,3
 function randomNumber() {
@@ -22,97 +17,80 @@ function getComputerChoice() {
         return "Scissors";
 }
 
-// function to get user input
-// function getUserChoice(){
-//     let userInput = prompt(`Welcome to Rock,Paper,Scissors game! \nPlease write your choice below`);
-//     let userInputLowerCase = userInput.toLowerCase();
-//     if (userInputLowerCase === "rock" )
-//         return "Rock";
-//     else if (userInputLowerCase === "paper" )
-//         return "Paper";
-//     else if (userInputLowerCase === "scissors" || userInputLowerCase === "scissor")
-//         return "Scissors";
-//     else
-//         return "Invalid choice";
-// }
-
 // function to play entire game of 5 rounds
 function playGame(userSelection) {
-    // for( i = 1 ; i < 6 ; i++ ) {
-    //     // let userSelection = getUserChoice();
-    //     let computerSelection = getComputerChoice();
-    //     console.log(playRound(userSelection, computerSelection));
-
-    //     // console.log(playRound(getUserChoice(),getComputerChoice()));
-    //     if (userSelection === "Invalid choice"){
-    //         --i;
-    //     }
-    // }
-
-        let computerSelection = getComputerChoice();
-        displayResult(playRound(userSelection, computerSelection));
-
+    let computerSelection = getComputerChoice();
+    displayResult(playRound(userSelection, computerSelection));
     displayResult("User Score : " + userScore + " Computer Score : " + computerScore );
-    winner(userScore,computerScore);
-
     // function to play single rounds
     function playRound(userChoice,computerChoice) {
         // If user chooses rock
         if (userChoice === "Rock"){
-            if (computerChoice === "Scissors"){
-                ++userScore;
-                displayPlayersChoice(userChoice,computerChoice);
-                return "You win! Rock beats Scissors";
-            }
-            else if (computerChoice === "Paper"){
-                ++computerScore;
-                displayPlayersChoice(userChoice,computerChoice);
-                return "You lose! Paper beats Rock";
-            }
-            else {
-                displayPlayersChoice(userChoice,computerChoice);
-                return "Its a tie! Play again";
-            }
+                if (computerChoice === "Scissors"){
+                    ++userScore;
+                    displayPlayersChoice(userChoice,computerChoice);
+                    return "You win! Rock beats Scissors";
+                }
+                else if (computerChoice === "Paper"){
+                    ++computerScore;
+                    displayPlayersChoice(userChoice,computerChoice);
+                    return "You lose! Paper beats Rock";
+                }
+                else {
+                    displayPlayersChoice(userChoice,computerChoice);
+                    return "Its a tie! Play again";
+                }
         }
         // If user chooses paper
         else if (userChoice === "Paper"){
-            if (computerChoice === "Scissors"){
-                ++computerScore;
-                displayPlayersChoice(userChoice,computerChoice);
-                return "You lose! Scissors beat Paper";
-            }
-            else if (computerChoice === "Rock"){
-                ++userScore;
-                displayPlayersChoice(userChoice,computerChoice);
-                return "You win! Paper beats Rock";
-            }
-            else {
-                displayPlayersChoice(userChoice,computerChoice);
-                return "Its a tie! Play again";
-            }
+                if (computerChoice === "Scissors"){
+                    ++computerScore;
+                    displayPlayersChoice(userChoice,computerChoice);
+                    return "You lose! Scissors beat Paper";
+                }
+                else if (computerChoice === "Rock"){
+                    ++userScore;
+                    displayPlayersChoice(userChoice,computerChoice);
+                    return "You win! Paper beats Rock";
+                }
+                else {
+                    displayPlayersChoice(userChoice,computerChoice);
+                    return "Its a tie! Play again";
+                }
         }
         // If user chooses scissors
-        else if (userChoice === "Scissors"){
-            if (computerChoice === "Paper"){
-                ++userScore;
-                displayPlayersChoice(userChoice,computerChoice);
-                return "You win! Scissors beat Paper";
+        else if(userChoice === "Scissors"){
+                if (computerChoice === "Paper"){
+                    ++userScore;
+                    displayPlayersChoice(userChoice,computerChoice);
+                    return "You win! Scissors beat Paper";
+                }
+                else if (computerChoice === "Rock"){
+                    ++computerScore;
+                    displayPlayersChoice(userChoice,computerChoice);
+                    return "You lose! Rock beats Scissors";
+                }
+                else {
+                    displayPlayersChoice(userChoice,computerChoice);
+                    return "Its a tie! Play again";
+                }
             }
-            else if (computerChoice === "Rock"){
-                ++computerScore;
-                displayPlayersChoice(userChoice,computerChoice);
-                return "You lose! Rock beats Scissors";
-            }
-            else {
-                displayPlayersChoice(userChoice,computerChoice);
-                return "Its a tie! Play again";
-            }
-        }
-        else {
-            displayPlayersChoice(userChoice,computerChoice);
-            return "Please enter a valid choice!";
-        }
     }
+    if (userScore === 5 || computerScore === 5)
+        {
+            const userHighScore = userScore;
+            const computerHighScore = computerScore;
+            userScore = 0;
+            computerScore = 0;
+            winner(userHighScore,computerHighScore);
+        }
+}
+
+// function to display final winner
+function winner(userHighScore,computerHighScore) {
+    (userHighScore > computerHighScore) ? displayResult(`Congrats! You win! Celebrate!`)
+    :(userHighScore < computerHighScore) ? displayResult(`You lose!\nBetter luck next time`)
+    :displayResult("Its a tie!")
 }
 
 //function displays players choice
@@ -126,6 +104,7 @@ function displayPlayersChoice(userChoice,computerChoice) {
 const choice = document.querySelector("#choice");
 const result = document.querySelector("#result");
 let userSelection = '';
+
 
 choice.addEventListener("click",(e)=> {
         let target = e.target;
